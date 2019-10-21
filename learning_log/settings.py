74bfa0744a9475207c247d4af270cd7a0c0fefa6
DNvrro <dsnavarro361@gmail.com>
2019-10-21 16:07:04 -0500

    Fixed allowed hosts

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j77()jz)=%q-(jux5amr9k3+)3ay-ay(i_l3&lg&d+nr=it3#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Allow only Heroku to host this project
-ALLOWED_HOSTS = ['learning-log361.heroku.app.com']
+ALLOWED_HOSTS = ['learning-log361.herokuapp.com']
 
 
 # Application definition
